be7d9ae4dc4c1b6aa5740b5d8d3d4d1d6eed13b6
Barbossa1 <57180014+Barbossa1@users.noreply.github.com>
2020-10-31 11:15:47 +0300

    Урок 4. Запросы пользователя и класс Request. Класс Response в фреймворке Laravel. Функциональное тестирование приложения

---
+++ lesson4/laravel/app/Http/Controllers/NewsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        return view('news.index', ['news' => $this->newsList]);
+    }
+
+    public function show(string $slug)
+    {
+        $name = null;
+        return view('news.show', ['slug' => $slug, 'name' => $name]);
+    }
+}
