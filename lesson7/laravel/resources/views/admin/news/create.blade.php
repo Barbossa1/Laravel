8fcf7d4b53aef3bd9888abb56bbbdfb5d53a4e7e
Barbossa1 <57180014+Barbossa1@users.noreply.github.com>
2020-11-11 17:45:34 +0300

    leeson7

---
+++ lesson7/laravel/resources/views/admin/news/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main')
+@section('content')
+    <div class="col-8 offset-2">
+        <h3>Добавление новости</h3>
+        <br>
+        <form method="post" action="{{ route('news.store') }}">
+            @csrf
+            <p>Заголовок: <br><input class="form-control" name="title" value="{{ old('title') }}" >
+            @error('title')
+                <div class="alert alert-danger">
+                    @foreach($errors->get('title') as $error)
+                        {{ $error }}
+                    @endforeach
+                </div>
+            @enderror</p>
+            <p>Автор: <br><input class="form-control" name="author" value="{{ old('author') }}" >
+            @error('author')
+            <div class="alert alert-danger">
+                @foreach($errors->get('author') as $error)
+                    {{ $error }}
+                @endforeach
+            </div>
+            @enderror</p>
+            <p>Описание: <br><textarea class="form-control" name="description">{!! old('description') !!}</textarea></p>
+            <button class="btn btn-success" type="submit">Добавить</button>
+        </form>
+    </div>
+@stop
